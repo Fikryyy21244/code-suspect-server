90729f9fcbee15e96fb331974e7bd63a59fdda01
Fikryyy21244 <mfikryhaikal1@gmail.com>
2026-03-14 21:05:27 +0700

    some changes

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -1,7 +1,10 @@
 import { Server } from "socket.io";
+import { roomController } from "../controllers/room.controller";
 
 export const initSocket = (io: Server) => {
   io.on("connection", (socket) => {
     console.log("user connected", socket.id);
+
+    roomController(socket);
   });
 };
